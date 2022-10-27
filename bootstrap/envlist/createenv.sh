87bbaca22bd5cc102a49d1e2cc504b3f7cc598c4
labs <labs.dwcts@gmail.com>
2022-10-27 17:51:26 +0900

    update

--- bootstrap/envlist/createenv.sh
+++ bootstrap/envlist/createenv.sh
@@ -4,9 +4,11 @@ set -e
 
 createEnv() {
   # create conda environments
-  conda env create --name tf-2.7 --file ~/setenv/tf-2.7.yml
-  conda env create --name pt-1.12.1 --file ~/setenv/pt-1.12.1.yml
-  conda env create --name sklearn-1.1.2 --file ~/setenv/sklearn-1.1.2.yml
+  /opt/tljh/user/bin/conda env list
+  
+  /opt/tljh/user/bin/conda env create --name "tf-2.7" --file "~/setenv/tf-2.7.yml"
+  /opt/tljh/user/bin/conda env create --name "pt-1.12.1" --file "~/setenv/pt-1.12.1.yml"
+  /opt/tljh/user/bin/conda env create --name "sklearn-1.1.2" --file "~/setenv/sklearn-1.1.2.yml"
 
   # conda environment list
   conda env list
