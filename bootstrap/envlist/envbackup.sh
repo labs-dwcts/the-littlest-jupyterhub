1a5811cd70de3d5014daa78f551953a9708748d5
labs <labs.dwcts@gmail.com>
2022-10-28 10:35:18 +0900

    update

--- bootstrap/envlist/envbackup.sh
+++ bootstrap/envlist/envbackup.sh
@@ -3,45 +3,36 @@
 set -e
 
 tfenvlist() {
-  # create a backup directory
-  mkdir ~/envlist
-  
   # conda environment list
   conda env list
   
   # backup conda environments
   conda activate tf-2.7
-  conda env export > ~/envlist/tf-2.7.yml
+  conda env export > ~/tf-2.7.yml
 
   # conda deactivate tf-2.7
   conda deactivate
 }
 
 ptenvlist() {
-  # create a backup directory
-  mkdir ~/envlist
-  
   # conda environment list
   conda env list
   
   # backup conda environments
   conda activate pt-1.12.1
-  conda env export > ~/envlist/pt-1.12.1.yml
+  conda env export > ~/pt-1.12.1.yml
 
   # conda deactivate pt-1.12.1
   conda deactivate
 }
 
 sklenvlist() {
-  # create a backup directory
-  mkdir ~/envlist
-  
   # conda environment list
   conda env list
   
   # backup conda environments
   conda activate sklearn-1.1.2
-  conda env export > ~/envlist/sklearn-1.1.2.yml
+  conda env export > ~/sklearn-1.1.2.yml
 
   # conda deactivate sklearn-1.1.2
   conda deactivate
