8767938769f1b5d4cc99fde43efdcb58bc6cf95e
labs <labs.dwcts@gmail.com>
2022-10-28 15:04:19 +0900

    update

--- bootstrap/envlist/createenv.sh
+++ bootstrap/envlist/createenv.sh
@@ -4,19 +4,19 @@ set -e
 
 createEnv() {
   # create conda environments
-  /opt/tljh/user/bin/conda env list
+  conda env list
 
-  /opt/tljh/user/bin/conda env create --name "tf-2.7" --file "~/setenv/tf-2.7.yml"
-  /opt/tljh/user/bin/conda env create --name "pt-1.12.1" --file "~/setenv/pt-1.12.1.yml"
-  /opt/tljh/user/bin/conda env create --name "sklearn-1.1.2" --file "~/setenv/sklearn-1.1.2.yml"
+  conda env create --name "tf-2.7" --file "~/setenv/tf-2.7.yml"
+  conda env create --name "pt-1.12.1" --file "~/setenv/pt-1.12.1.yml"
+  conda env create --name "sklearn-1.1.2" --file "~/setenv/sklearn-1.1.2.yml"
 
   # conda environment list
-  /opt/tljh/user/bin/conda env list
+  conda env list
 }
 addEnv() {
-  /opt/tljh/user/bin/python -m ipykernel install --user --name "tf-2.7" --display-name="tf-2.7"
-  /opt/tljh/user/bin/python -m ipykernel install --user --name "pt-1.12.1" --display-name="pt-1.12.1"
-  /opt/tljh/user/bin/python -m ipykernel install --user --name "sklearn-1.1.2" --display-name="sklearn-1.1.2"
+  python -m ipykernel install --user --name "tf-2.7" --display-name="tf-2.7"
+  python -m ipykernel install --user --name "pt-1.12.1" --display-name="pt-1.12.1"
+  python -m ipykernel install --user --name "sklearn-1.1.2" --display-name="sklearn-1.1.2"
 }
 
 createEnv
