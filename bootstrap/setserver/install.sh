5336888d327031586be37d493120ce99ddd2543c
labs <labs.dwcts@gmail.com>
2022-11-02 14:55:08 +0900

    update

--- bootstrap/setserver/install.sh
+++ bootstrap/setserver/install.sh
@@ -76,6 +76,6 @@ wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 # source ~/.bashrc
-go version
+/usr/local/go/bin/go version
 
 echo 'reboot & test'
