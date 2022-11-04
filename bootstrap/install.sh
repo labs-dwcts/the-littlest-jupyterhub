ebc83175d3540f53e5a04a267127dded1c889cc3
labs <labs.dwcts@gmail.com>
2022-11-04 12:01:50 +0900

    update

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+
 #
 # install jupyterHub (bare metal)
 #
@@ -14,6 +15,7 @@ doSetDirectory() {
   skelscratch="/etc/skel/scratch"
   jupyterhubusers="jupyterhub-users"
 
+
   # change group ownership and default permissions to use group
   if [ ! $(getent group "${jupyterhubusers}") ]
     then
@@ -21,6 +23,7 @@ doSetDirectory() {
   else
     echo "${jupyterhubusers} group exists."
   fi
+
 
   # create a share directory
   for value in $data $setenv $scratch
@@ -33,6 +36,7 @@ doSetDirectory() {
       fi
   done
   
+
   # create a symlink $data, $setenv, $scratch
   if [ ! -L "${skeldata}" ]
     then
@@ -49,6 +53,7 @@ doSetDirectory() {
       sudo ln -s "${scratch}" "${skelscratch}"
   fi
 
+
   # update permissions
   sudo chown -R root:jupyterhub-users "${scratch}" "${setenv}"
   sudo chmod 777 "${scratch}"
@@ -60,35 +65,43 @@ doInstall() {
   # update
   sudo apt update
   
+
   # prerequisites
   sudo apt install -y build-essential
   sudo apt install -y python3 python3-dev git curl
   curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt install -y nodejs
   
+  
   # installation
   curl -L https://raw.githubusercontent.com/labs-dwcts/the-littlest-jupyterhub/main/bootstrap/bootstrap.py | sudo -E python3 - --admin admin
   
+
   # change jupyterhub directory
   git clone https://github.com/labs-dwcts/the-littlest-jupyterhub temp
   sudo cp -fr temp/share/jupyterhub/ /opt/tljh/hub/share/
   
+
   # add conda environment
   sudo cp -fr temp/bootstrap/envlist/* /srv/setenv
   
   rm -fr temp
 
+
   # settings
   # change default user interface for users
   sudo tljh-config set user_environment.default_app jupyterlab
   
+
   # enable PAM authenticator
   # sudo tljh-config set auth.type jupyterhub.auth.PAMAuthenticator
   sudo tljh-config reload
   
+
   # jupyterlab ko-KR language pack
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge -y \
     jupyterlab-language-pack-ko-KR
   
+
   # extension
   # conda-forge
   sudo -E /opt/tljh/user/bin/conda install -c conda-forge -y \
@@ -103,6 +116,7 @@ doInstall() {
     ipywidgets \
     jupyter-archive
   
+
   # pypi
   sudo -E /opt/tljh/user/bin/pip install \
     jupyterlab-nvdashboard \
